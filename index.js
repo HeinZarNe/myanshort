02c1b3f5fb9545d38dc6127ef1c8ccb026934c9a
HeinZarNe <heinzarne2522005@gmail.com>
2024-12-20 11:37:33 +0630

    Fix

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const shortid = require("shortid");
 const validator = require("validator");
-require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then((_) => console.log("Mongodb is connected"))
-  .catch((err) => console.error(err));
+  .then(() => console.log("Mongodb is connected"))
+  .catch((err) => console.error("Mongodb connection error:", err));
 
 const urlSchema = new mongoose.Schema({
   shortId: { type: String },
@@ -28,7 +28,7 @@ app.get("/", async (req, res) => {
   res.json("Hello");
 });
 
-//POST
+// POST
 app.post("/shorten", async (req, res) => {
   const { originalUrl } = req.body;
   if (!validator.isURL(originalUrl)) {
@@ -43,15 +43,15 @@ app.post("/shorten", async (req, res) => {
     res.json({ shortId });
   } catch (err) {
     if (err.code === 11000) {
-      res.status(409).json({ message: "Url already exist" });
+      res.status(409).json({ message: "Url already exists" });
     } else {
-      console.error(err);
+      console.error("Error saving URL:", err);
       res.status(500).json({ message: "Internal Server Error" });
     }
   }
 });
 
-//Get
+// GET
 app.get("/:shortId", async (req, res) => {
   const { shortId } = req.params;
   try {
@@ -64,7 +64,7 @@ app.get("/:shortId", async (req, res) => {
       res.status(404).json({ message: "URL not found" });
     }
   } catch (err) {
-    console.error(err);
+    console.error("Error finding URL:", err);
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
